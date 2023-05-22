2d4a49e900c565429823c9cde0367a0ab0fc4a4f
ShehzorMemon <Shezymemon0180@gmail.com>
2023-05-22 16:55:27 +0500

    Ready to preview.

---
+++ src/components/Data.jsx
@@ -0,0 +1,290 @@
+const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+        question: "Who won the 1992 cricket world cup?",
+        answers: [
+          {
+            text: "India",
+            correct: false,
+          },
+          {
+            text: "Australia",
+            correct: false,
+          },
+          {
+            text: "Westindies",
+            correct: false,
+          },
+          {
+            text: "Pakistan",
+            correct: true,
+          },
+        ],
+    },
+    {
+        id: 5,
+          question: "Which country is known as the Land of the Rising Sun?",
+          answers: [
+            {
+              text: "China",
+              correct: false,
+            },
+            {
+              text: "Japan",
+              correct: true,
+            },
+            {
+              text: "South Korea",
+              correct: false,
+            },
+            {
+              text: "Thailand",
+              correct: false,
+            },
+        ],
+    },
+    {
+        id: 6,
+          question: "What is the largest ocean in the world?",
+          answers: [
+            {
+              text: "Atlantic Ocean",
+              correct: false,
+            },
+            {
+              text: "Indian Ocean",
+              correct: false,
+            },
+            {
+              text: "Pacific Ocean",
+              correct: true,
+            },
+            {
+              text: "Arctic Ocean",
+              correct: false,
+            },
+        ],
+    },
+    {
+        id: 7,
+          question: "Which planet is known as the Red Planet?",
+          answers: [
+            {
+              text: "Jupiter",
+              correct: false,
+            },
+            {
+              text: "Venus",
+              correct: false,
+            },
+            {
+              text: "Mars",
+              correct: true,
+            },
+            {
+              text: "Saturn",
+              correct: false,
+            },
+        ],
+    },
+    {
+        id: 8,
+          question: "What is the capital city of Australia?",
+          answers: [
+            {
+              text: "Sydney",
+              correct: false,
+            },
+            {
+              text: "Canberra",
+              correct: true,
+            },
+            {
+              text: "Melbourne",
+              correct: false,
+            },
+            {
+              text: "Perth",
+              correct: false,
+            },
+        ],
+    },
+    {
+        id: 9,
+          question: "Which African country is known as the Rainbow Nation?",
+          answers: [
+            {
+              text: "Kenya",
+              correct: false,
+            },
+            {
+              text: "Nigeria",
+              correct: false,
+            },
+            {
+              text: "South Africa",
+              correct: true,
+            },
+            {
+              text: "Zimbabwe",
+              correct: false,
+            },
+        ],
+    },
+    {
+        id: 10,
+          question: "Which continent is home to the Amazon Rainforest?",
+          answers: [
+            {
+              text: "Africa",
+              correct: false,
+            },
+            {
+              text: "South America",
+              correct: true,
+            },
+            {
+              text: "Asia",
+              correct: false,
+            },
+            {
+              text: "Europe",
+              correct: false,
+            },
+        ],
+    },
+    {
+        id: 11,
+          question: "Which mountain range is located in North America?",
+          answers: [
+            {
+              text: "Andes",
+              correct: false,
+            },
+            {
+              text: "Himalayas",
+              correct: false,
+            },
+            {
+              text: "Rocky Mountains",
+              correct: true,
+            },
+            {
+              text: "Alps",
+              correct: false,
+            },
+        ],
+    },
+    {
+        id: 12,
+          question: "Which country is known for its famous fjords?",
+          answers: [
+            {
+              text: "Norway",
+              correct: true,
+            },
+            {
+              text: "Sweden",
+              correct: false,
+            },
+            {
+              text: "Iceland",
+              correct: false,
+            },
+            {
+              text: "New Zealand",
+              correct: false,
+            },
+        ],
+    },
+    {
+        id: 13,
+          question: "Who painted the famous artwork Mona Lisa?",
+          answers: [
+            {
+              text: "Vincent van Gogh",
+              correct: false,
+            },
+            {
+              text: "Pablo Picasso",
+              correct: false,
+            },
+            {
+              text: "Leonardo da Vinci",
+              correct: true,
+            },
+            {
+              text: "Claude Monet",
+              correct: false,
+            },
+        ],
+    },
+  ];
+
+  export default data;
